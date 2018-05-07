2ca1a99146e10b475b1dcbbf12e12b30800aa65d
mansourjaber <37308422+mansourjaber@users.noreply.github.com>
2018-05-07 22:06:05 +0100

    Update 10.js

--- More-JavaScript/10.js
+++ More-JavaScript/10.js
@@ -5,7 +5,29 @@ Hint, the output should be correct English with all the punctuation in place (th
 So plurals for the vehicle types, commas followed by a single space,
 the word and to replace the final comma and closed off by a period.*/
 "use strict";
+function advertisement(cars) {
+        
+        let thingsWeService = '';
 
+        let index = 0;
+
+        for(let i = 0; i < cars.length; i++ ) {
+            
+            let car = cars[i];
+            let pluralForm = car + 's';
+
+            if (i == cars.length - 1) {
+                thingsWeService += 'and ' + pluralForm;
+            }
+            else 
+            {
+                thingsWeService += pluralForm + ', ';
+            }
+        }
+
+        console.log('Welcome to Amazing Joe\'s Garage, we service ' + thingsWeService);
+    }
+/*
 let vehicleType = ["car", "motorbike", "caravan", "bike"]
 for (let i = 0; i < vehicleType.length; i++) {
     vehicleType[i] = vehicleType[i] + 's';
@@ -13,5 +35,5 @@ for (let i = 0; i < vehicleType.length; i++) {
 let text = vehicleType.join(", ");
 let advertisement = text.replace("caravans,", "caravans and");
 
-console.log("Amazing Joe's Garage, we service" + " " + advertisement + ".");
+console.log("Amazing Joe's Garage, we service" + " " + advertisement + ".");*/
 // this is a static code it does not work
